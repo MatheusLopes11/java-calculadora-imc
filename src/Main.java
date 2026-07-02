0a8414136cf190382b1a61bf99eb075d32c61b7d
Matheus <matheus.lopes6@aluno.cps.sp.gov.br>
2026-07-01 21:00:42 -0300

    commit inicial

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("------------------");
+        System.out.println("CALCULADORA DE IMC");
+        System.out.println("------------------\n");
+
+        System.out.println("Digite seu nome: ");
+        String nome = entrada.nextLine();
+        System.out.println();
+
+        System.out.printf("Olá %s! Bora calcular o seu IMC?\n\n", nome);
+
+        System.out.println("Primeiro, digite o seu peso em kg: \n");
+        double peso = entrada.nextDouble();
+        System.out.println();
+
+        System.out.println("Beleza, agora digita a sua altura em metros: \n");
+        double altura = entrada.nextDouble();
+        System.out.println();
+
+        System.out.println("Processando... \n");
+
+        double imc = peso / (Math.pow(altura, altura));
+
+        System.out.printf("Resultado: ≈ %.2f kg/m²", imc);
+
+        entrada.close();
+
+    }
+
+}
